f1a534090b547afecb51ba082c1380d91d784891
宋来瑞 <cnre@live.cn>
2019-11-28 13:27:43 +0800

    feat(module): @vue/ui register components

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./plugins";
+import "./register-components";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import { createProvider } from "./vue-apollo";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
---
+++ src/plugins.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import VueUi from "@vue/ui";
+
+import "@vue/ui/dist/vue-ui.css";
+
+Vue.use(VueUi);

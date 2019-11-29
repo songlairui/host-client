f11fec4addeae9cc551185960a5645047202b9bf
宋来瑞 <cnre@live.cn>
2019-11-29 18:45:43 +0800

    feat(nav): add min max ize

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <VueButton class="flat icon-button" icon-left="zoom_out_map" @click="maxium" />
+      <VueButton class="flat icon-button" icon-left="remove" @click="minize" />
+      <VueButton class="flat icon-button" icon-left="home" to="/" />
+      <VueButton class="flat icon-button" icon-left="settings" to="/about" />
     </div>
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  name: "App",
+  methods: {
+    maxium() {
+      window.resizeTo(440, 660);
+    },
+    minize() {
+      window.resizeTo(80, 30);
+    }
+  }
+};
+</script>
 <style lang="stylus">
 html, body, #app {
   height: 100%;
@@ -32,7 +46,7 @@ html, body, #app {
     flex: 0 0 auto;
     -webkit-user-select: none;
     -webkit-app-region: drag;
-    padding: .3em 2em 0 0;
+    padding: 0.3em 2em 0 0;
     cursor: move;
   }
 }
